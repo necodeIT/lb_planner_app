565dc8fdb672b7b21024b160ecda81cc6667e7e6
McQuenji <60017181+mcquenji@users.noreply.github.com>
2024-07-31 23:41:42 +0200

    feat: store auth state on disk

--- lib/modules/auth/auth.dart
+++ lib/modules/auth/auth.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:lb_planner/modules/auth/auth.dart';
 import 'package:lb_planner/modules/auth/infra/services/moodle_auth_service.dart';
 import 'package:mcquenji_core/mcquenji_core.dart';
+import 'package:mcquenji_local_storage/mcquenji_local_storage.dart';
 
 export 'domain/domain.dart';
 export 'presentation/presentation.dart';
@@ -12,19 +13,24 @@ class AuthModule extends Module {
   @override
   List<Module> get imports => [
         CoreModule(),
+        LocalStorageModule(),
       ];
 
   @override
   void binds(Injector i) => i
     ..add<AuthService>(MoodleAuthService.new)
     ..addRepository(AuthRepository.new)
-    ..addRepository(UserRepository.new);
+    ..addRepository(UserRepository.new)
+    ..addSerde<Token>(fromJson: Token.fromJson, toJson: (t) => t.toJson())
+    ..addSerde<User>(fromJson: User.fromJson, toJson: (u) => u.toJson());
 
   @override
   void exportedBinds(Injector i) => i
     ..add<AuthService>(MoodleAuthService.new)
     ..addRepository(UserRepository.new)
-    ..addRepository(AuthRepository.new);
+    ..addRepository(AuthRepository.new)
+    ..addSerde<Token>(fromJson: Token.fromJson, toJson: (t) => t.toJson())
+    ..addSerde<User>(fromJson: User.fromJson, toJson: (u) => u.toJson());
 
   @override
   void routes(RouteManager r) {}
--- lib/modules/auth/presentation/repositories/auth_repository.dart
+++ lib/modules/auth/presentation/repositories/auth_repository.dart
@@ -1,23 +1,51 @@
 import 'package:lb_planner/modules/auth/auth.dart';
 import 'package:mcquenji_core/mcquenji_core.dart';
+import 'package:mcquenji_local_storage/mcquenji_local_storage.dart';
 
 /// UI state controller for authentication.
 ///
 /// If the state is an empty set, the user is not authenticated.
 class AuthRepository extends Repository<AsyncValue<Set<Token>>> {
   final AuthService _auth;
+  final LocalStorageDatasource _localStorage;
 
   /// UI state controller for authentication.
-  AuthRepository(this._auth) : super(AsyncValue.loading());
+  AuthRepository(this._auth, this._localStorage) : super(AsyncValue.loading()) {
+    _authFromStorage();
+  }
+
+  Future<void> _authFromStorage() async {
+    if (!await _localStorage.exists<Set<Token>>()) {
+      log('No token found in storage');
+
+      return;
+    }
+
+    final tokens = await _localStorage.read<Set<Token>>();
+
+    for (final token in tokens) {
+      final valid = await _auth.verifyToken(token);
+
+      if (!valid) {
+        log('Token $token is invalid. Setting state to empty.');
+
+        await logout();
+
+        return;
+      }
+    }
+
+    data(tokens);
+  }
 
   /// Sign in with [username] and [password].
-  Future<void> login({
+  Future<void> authenticate({
     required String username,
     required String password,
   }) async {
     emit(AsyncValue.loading());
 
-    final state = await AsyncValue.guard(
+    await guard(
       () => _auth.authenticate(
         username: username,
         password: password,
@@ -25,17 +53,22 @@ class AuthRepository extends Repository<AsyncValue<Set<Token>>> {
       ),
     );
 
-    emit(state);
+    if (!state.hasData) return;
+
+    await _localStorage.write(state.requireData);
   }
 
   /// Sign out.
   Future<void> logout() async {
-    emit(AsyncValue.data({}));
+    data({});
+
+    await _localStorage.delete<Set<Token>>();
   }
 
   @override
   void dispose() {
     super.dispose();
     _auth.dispose();
+    _localStorage.dispose();
   }
 }
