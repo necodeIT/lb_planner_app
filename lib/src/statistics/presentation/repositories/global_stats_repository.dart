e8f6df594a599dff911ca397c9164c821b43671d
mcquenji <benjamin.mceachnie@necode.it>
2025-02-06 14:06:45 +0000

    fix: respect user settings in global stats

--- lib/src/statistics/presentation/repositories/global_stats_repository.dart
+++ lib/src/statistics/presentation/repositories/global_stats_repository.dart
@@ -7,23 +7,27 @@ import 'package:mcquenji_core/mcquenji_core.dart';
 /// Repository responsible for providing global statistics for all tasks.
 class GlobalStatsRepository extends Repository<AsyncValue<TaskAggregate>> {
   final MoodleTasksRepository _tasks;
+  final UserRepository _user;
 
   /// Repository responsible for providing global statistics for all tasks.
-  GlobalStatsRepository(this._tasks) : super(AsyncValue.loading()) {
+  GlobalStatsRepository(this._tasks, this._user) : super(AsyncValue.loading()) {
     watchAsync(_tasks);
+    watchAsync(_user);
   }
 
   @override
   FutureOr<void> build(BuildTrigger trigger) async {
     if (!_tasks.state.hasData) return;
 
+    final user = waitForData(_user);
+
     data(
       TaskAggregate.fromTasks(
         _tasks.filter(
           type: {
             MoodleTaskType.required,
-            MoodleTaskType.optional,
             MoodleTaskType.participation,
+            if (user.optionalTasksEnabled) MoodleTaskType.optional,
           },
         ),
       ),
