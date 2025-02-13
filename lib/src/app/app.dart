4f64f6b81e0abc45ccedbb718d725256ded54edf
mcquenji <benjamin.mceachnie@necode.it>
2025-02-13 22:51:45 +0100

    fix: reenable licensing

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,8 +1,10 @@
 library lb_planner.modules.app;
 
 import 'package:animations/animations.dart';
+import 'package:echidna_flutter/echidna_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:lb_planner/config/echidna.dart';
 import 'package:lb_planner/config/version.dart';
 import 'package:lb_planner/lb_planner.dart';
 import 'package:mcquenji_core/mcquenji_core.dart';
@@ -73,7 +75,7 @@ class AppModule extends Module {
             customTransition: defaultTransition,
             guards: [
               CapabilityGuard({UserCapability.student}, redirectTo: '/slots/'),
-              // FeatureGuard([kCalendarPlanFeatureID], redirectTo: '/settings/'),
+              FeatureGuard([kCalendarPlanFeatureID], redirectTo: '/settings/'),
             ],
           ),
           ModuleRoute(
