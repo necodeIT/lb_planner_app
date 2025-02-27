52d9b2a9b121866c65e144783bfdf172ed3c89d1
mcquenji <benjamin.mceachnie@necode.it>
2025-02-27 11:37:53 +0100

    feat: add privacy policy and subscription management options to general settings

--- lib/src/settings/presentation/widgets/general_settings.dart
+++ lib/src/settings/presentation/widgets/general_settings.dart
@@ -1,10 +1,13 @@
 import 'package:awesome_extensions/awesome_extensions.dart';
+import 'package:eduplanner/config/posthog.dart';
 import 'package:eduplanner/config/version.dart';
 import 'package:eduplanner/src/app/app.dart';
 import 'package:eduplanner/src/auth/auth.dart';
 import 'package:eduplanner/src/theming/theming.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Displays the general settings of the app.
 class GeneralSettings extends StatefulWidget {
@@ -81,6 +84,7 @@ class _GeneralSettingsState extends State<GeneralSettings> {
                     icon: Icons.update,
                     onPressed: checkUpdates,
                   ),
+
                   // item(context.t.settings_general_deleteProfile, Icons.delete, deleteProfile, context.theme.colorScheme.error),
                   if (isStudent)
                     checkboxItem(
@@ -94,6 +98,18 @@ class _GeneralSettingsState extends State<GeneralSettings> {
                       value: user.state.data?.displayTaskCount ?? false,
                       onChanged: user.setDisplayTaskCount,
                     ),
+                  iconItem(
+                    title: 'Privacy Policy', // TODO(MasterMarcoHD): Localize
+                    icon: FontAwesome5Solid.balance_scale,
+                    onPressed: () => launchUrl(kPrivacyPolicyUrl),
+                    iconSize: 14,
+                  ),
+                  iconItem(
+                    title: 'Manage Subscription', // TODO(MasterMarcoHD): Localize
+                    icon: FontAwesome5Solid.credit_card,
+                    onPressed: () => Modular.to.pushNamed('/subscription'), // TODO(mcquenji): Implement subscription screen
+                    iconSize: 14,
+                  ),
                 ].vSpaced(Spacing.smallSpacing),
               ),
             ),
@@ -103,14 +119,15 @@ class _GeneralSettingsState extends State<GeneralSettings> {
     );
   }
 
-  Widget iconItem({required String title, required IconData icon, VoidCallback? onPressed, Color? hoverColor}) {
+  Widget iconItem({required String title, required IconData icon, VoidCallback? onPressed, Color? hoverColor, double? iconSize = 20}) {
     return item(
       title: title,
       suffix: HoverBuilder(
         onTap: onPressed,
         builder: (context, hovering) {
           return Container(
-            padding: PaddingAll(Spacing.xsSpacing),
+            height: 35,
+            width: 35,
             decoration: ShapeDecoration(
               shape: squircle(),
               color: context.theme.scaffoldBackgroundColor,
@@ -118,6 +135,7 @@ class _GeneralSettingsState extends State<GeneralSettings> {
             child: Icon(
               icon,
               color: hovering ? hoverColor ?? context.theme.colorScheme.primary : context.theme.colorScheme.onSurface,
+              size: iconSize,
             ),
           );
         },
@@ -139,13 +157,19 @@ class _GeneralSettingsState extends State<GeneralSettings> {
 
   // ignore: avoid_positional_boolean_parameters
   Widget item({required String title, required Widget suffix, VoidCallback? onPressed}) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(title).expanded(),
-        Spacing.xs(),
-        suffix,
-      ],
+    return SizedBox(
+      height: 35,
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title).expanded(),
+            Spacing.xs(),
+            suffix,
+          ],
+        ),
+      ),
     );
   }
 }
