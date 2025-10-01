dc401b93fd6709933d95bb5cc6ee6403de58c104
mcquenji <benjamin.mceachnie@necode.it>
2025-10-01 07:55:58 +0200

    chore: add slot view switcher widget

--- lib/src/slots/presentation/widgets/widgets.dart
+++ lib/src/slots/presentation/widgets/widgets.dart
@@ -1,6 +1,7 @@
 export 'edit_slot_dialog.dart';
 export 'number_spinner.dart';
+export 'slot_data_pop_over.dart';
 export 'slot_master_widget.dart';
 export 'slot_overview_widget.dart';
 export 'slot_widget.dart';
-export 'slot_data_pop_over.dart';
+export 'slots_view_switcher.dart';
